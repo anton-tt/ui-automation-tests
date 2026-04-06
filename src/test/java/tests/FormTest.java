81e422ba239d41830790ba4255f8a032a037afc2
anton-tt <carduelis@yandex.ru>
2026-04-06 16:26:20 +0700

    refactor: вынесен код настройки WebDriver в класс BaseTest

--- src/test/java/tests/FormTest.java
+++ src/test/java/tests/FormTest.java
@@ -1,44 +1,35 @@
 package tests;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import base.BaseTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Alert;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.FormPage;
 
 import java.time.Duration;
 
-public class FormTest {
+public class FormTest extends BaseTest {
 
     @Test
     void positiveFormTest() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        try {
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-            FormPage form = new FormPage(driver);
-            String[] tools = {"Selenium", "WebDriverManager", "JUnit", "Maven"};
-            form.open()
-                    .enterName("Anton")
-                    .enterPassword("12345")
-                    .selectMilk()
-                    .selectCoffee()
-                    .selectYellowColor()
-                    .selectAutomationYes()
-                    .enterEmail("test@example.com")
-                    .enterMessageWithTools(tools)
-                    .submitForm();
-            Alert alert = wait.until(ExpectedConditions.alertIsPresent());
-            Assertions.assertEquals("Message received!", alert.getText());
-            alert.accept();
-
-        } finally {
-            driver.quit();
-        }
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        FormPage form = new FormPage(driver);
+        String[] tools = {"Selenium", "WebDriverManager", "JUnit", "Maven"};
+        form.open()
+                .enterName("Anton")
+                .enterPassword("12345")
+                .selectMilk()
+                .selectCoffee()
+                .selectYellowColor()
+                .selectAutomationYes()
+                .enterEmail("test@example.com")
+                .enterMessageWithTools(tools)
+                .submitForm();
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+        Assertions.assertEquals("Message received!", alert.getText());
+        alert.accept();
     }
 
 }
